7965f1dc2193e6fb8d1707fded17b80965859739
César Alonso <isaquealosnso@gmail.com>
2025-03-12 03:48:35 -0300

    desafio01
    trabalhei no desafio01

--- aula05/desafio01/javaScript.js
+++ aula05/desafio01/javaScript.js
@@ -1,28 +1,40 @@
+let num_digitados = document.getElementById('num_digitados')
+let num_quantidade = document.getElementById('num_quantidade')
+let num_ordenado = document.getElementById('num_ordenado')
+let index_10 = document.getElementById('index_10')
+
+function dados_organizadados() {
+
     let quantidade = parseInt(prompt("Quantos dados você deseja inserir?"));
-    
-    // Inicializa o array que vai armazenar os dados
+
     let dados = [];
-    
+
     // Loop para coletar os dados digitados pelo usuário
     for (let i = 0; i < quantidade; i++) {
         let dado = parseInt(prompt(`Digite o dado ${i + 1}:`));
-        dados.push(dado);  // Adiciona o dado no array
+        dados.push(dado);
     }
-    
+
+    // Adiciona os numeros digitados
+    num_digitados.innerText = dados.join("  ,  ");
+
     // Adiciona o número 10 no final do array
     dados.push(10);
-    
+
     // Ordena o array em ordem crescente
     dados.sort((a, b) => a - b); // decresente: b - a
+
+    // Mostrar informações no html
+    num_quantidade.innerText = "Você digitou " + quantidade + " números";
+    num_ordenado.innerText = dados.join("  ,  ");
+    index_10.innerText = dados.indexOf(10);
+
+    /* // Exibe o array ordenado
+    alert("Dados ordenados: " + dados.join(", "));
     
-    // Exibe o array ordenado
-    alert("Dados ordenados: " + dados.join(", "));//O método join() é usado para juntar os elementos de um array em uma única string, separando-os por um valor especificado, que pode ser uma vírgula, espaço, ou qualquer outro caractere.
-    
-    // Exibe a quantidade de dados inseridos pelo usuário (antes de adicionar o 10)
+    // Exibe a quantidade de dados inseridos pelo usuário 
     alert("Você digitou " + quantidade + " números.");
     
-    // encontrar o índice do número 10 no array
-    let indiceDeDez = dados.indexOf(10);
-    
     // Exibe o índice do número 10 no array
-    alert("O número 10 está no índice: " + indiceDeDez);
+    alert("O número 10 está no índice: " + dados.indexOf(10));*/3
+}
